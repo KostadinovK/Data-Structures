78e0574e5faf3005dd14f6ea127c33fe1267174c
KostadinovK <kostadinovk2001@gmail.com>
2018-12-03 16:03:50 +0200

    Finished lab for lection 01

---
+++ 01-Linear-Data-Structures-List-And-DS-Complexity/Linear-Data-Structures-Lab/Lists/ArrayList.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+public class ArrayList<T>
+{
+	private T[] data;
+
+	private void Resize()
+	{
+		T[] newArray = new T[data.Length * 2];
+		Array.Copy(data,newArray,data.Length);
+		data = newArray;
+	}
+
+	private void Shrink()
+	{
+		T[] newArray = new T[data.Length/2];
+		Array.Copy(data,newArray,Count);
+		data = newArray;
+	}
+
+	private void Shift(int index)
+	{
+		for (int i = index; i < Count; i++)
+		{
+			data[i] = data[i + 1];
+		}
+	}
+
+	public int Count { get; private set; }
+
+	public ArrayList()
+	{
+		data = new T[2];
+		Count = 0;
+	}
+
+	public T this[int index]
+    {
+        get
+        {
+	        if (index < 0 || index >= Count)
+	        {
+				throw new ArgumentOutOfRangeException();
+	        }
+
+	        return data[index];
+        }
+
+        set
+        {
+	        if (index < 0 || index >= Count)
+	        {
+		        throw new ArgumentOutOfRangeException();
+	        }
+			data[index] = value;
+        }
+    }
+
+    public void Add(T item)
+    {
+	    if (Count >= data.Length)
+	    {
+		    Resize();
+	    }
+
+	    data[Count++] = item;
+    }
+
+    public T RemoveAt(int index)
+    {
+	    if (index < 0 || index >= Count)
+	    {
+		    throw new ArgumentOutOfRangeException();
+	    }
+
+	    T elementToRemove = data[index];
+	    data[index] = default(T);
+	    Shift(index);
+	    Count--;
+	    if (data.Length / 4 >= Count)
+	    {
+		    Shrink();
+	    }
+
+	    return elementToRemove;
+    }
+}
