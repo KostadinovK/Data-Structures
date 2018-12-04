ce1ba5be2ff13b52f606800ffa60f56cdf7ff416
KostadinovK <kostadinovk2001@gmail.com>
2018-12-04 16:56:14 +0200

    Solved all tasks from homework for lection 01

---
+++ 01-Linear-Data-Structures-List-And-DS-Complexity/Linear-Data-Structures-Homework/07-Distance-In-Labyrinth/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Tile
+{
+	public int Row { get; private set; }
+	public int Col { get; private set; }
+
+	public int Number { get; private set; }
+
+	public Tile(int row, int col, int number)
+	{
+		Row = row;
+		Col = col;
+		Number = number;
+	}
+}
+
+public class Program
+{
+	
+	private static void SetUnreachableTiles(string[,] maze,int size)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (maze[i,j] == "0")
+				{
+					maze[i, j] = "u";
+				}
+			}
+		}
+
+	}
+	
+	private static void CheckInFourDirections(Tile tile,string[,] maze, int size,List<Tile> tilesToCheck)
+	{
+		int row = tile.Row;
+		int col = tile.Col;
+		int number = tile.Number;
+
+		//Up
+		if (row-1 >= 0 && maze[row-1,col] == "0")
+		{
+			Tile upperTile = new Tile(row-1,col,number+1);
+			maze[row - 1, col] = (number + 1).ToString();
+			tilesToCheck.Add(upperTile);
+		}
+
+		//Right
+		if (col+1 < size && maze[row,col+1] == "0")
+		{
+			Tile rightTile = new Tile(row, col+1, number + 1);
+			maze[row, col+1] = (number + 1).ToString();
+			tilesToCheck.Add(rightTile);
+		}
+
+		//Down
+		if (row+1 < size && maze[row+1,col] == "0")
+		{
+			Tile downTile = new Tile(row + 1, col, number + 1);
+			maze[row + 1, col] = (number + 1).ToString();
+			tilesToCheck.Add(downTile);
+		}
+
+		//Left
+		if (col-1 >= 0 && maze[row,col-1] == "0")
+		{
+			Tile leftTile = new Tile(row, col-1, number + 1);
+			maze[row, col-1] = (number + 1).ToString();
+			tilesToCheck.Add(leftTile);
+		}
+	}
+
+	public static void Main()
+	{
+		int size = int.Parse(Console.ReadLine());
+
+		string[,] maze = new string[size,size];
+
+		int startingRow = 0;
+		int startingCol = 0;
+		for (int i = 0;i < size;i++)
+		{
+			string line = Console.ReadLine();
+			for (int j = 0;j < size;j++)
+			{
+				maze[i, j] = line[j].ToString();
+				if (line[j] == '*')
+				{
+					startingRow = i;
+					startingCol = j;
+				}
+			}
+		}
+
+		List<Tile> tilesToCheck = new List<Tile>();
+		Tile start = new Tile(startingRow,startingCol,0);
+		tilesToCheck.Add(start);
+
+		while (tilesToCheck.Count != 0)
+		{
+			
+			int endIndex = tilesToCheck.Count;
+			for (int i = 0; i < endIndex; i++)
+			{
+				CheckInFourDirections(tilesToCheck[i],maze, size, tilesToCheck);
+			}
+
+			for (int i = 0; i < endIndex; i++)
+			{
+				tilesToCheck.RemoveAt(0);
+			}
+		}
+
+		SetUnreachableTiles(maze, size);
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				Console.Write(maze[i,j]);
+			}
+
+			Console.WriteLine();
+		}
+
+
+	}
+}
---
+++ 01-Linear-Data-Structures-List-And-DS-Complexity/Linear-Data-Structures-Homework/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoublyLinkedList<T> : IEnumerable<T>
+{
+	private class Node<T>
+	{
+		public Node<T> Previous { get; set; }
+		public T Value { get; set; }
+		public Node<T> Next { get; set; }
+
+		public Node(T value)
+		{
+			Value = value;
+			Previous = null;
+			Next = null;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+
+	public int Count { get; private set; }
+
+    public void AddFirst(T element)
+    {
+	    if (Count == 0)
+	    {
+		    head = tail = new Node<T>(element);
+	    }
+	    else
+	    {
+		    Node<T> newHead = new Node<T>(element);
+		    newHead.Next = head;
+		    head.Previous = newHead;
+		    head = newHead;
+	    }
+
+	    Count++;
+    }
+
+    public void AddLast(T element)
+    {
+	    if (Count == 0)
+	    {
+		    head = tail = new Node<T>(element);
+	    }
+	    else
+	    {
+		    Node<T> newTail = new Node<T>(element);
+			newTail.Previous = tail;
+		    tail.Next = newTail;
+		    tail = newTail;
+	    }
+
+	    Count++;
+    }
+
+    public T RemoveFirst()
+    {
+	    if (Count == 0)
+	    {
+		    throw new InvalidOperationException("List empty");
+	    }
+
+	    T valueToRemove = head.Value;
+	    head = head.Next;
+	    if (head != null)
+	    {
+		    head.Previous = null;
+		}
+	    else
+	    {
+		    tail = null;
+	    }
+
+
+	    Count--;
+
+	    return valueToRemove;
+    }
+
+    public T RemoveLast()
+    {
+		if (Count == 0)
+	    {
+		    throw new InvalidOperationException("List empty");
+	    }
+
+	    T valueToRemove = tail.Value;
+	    tail = tail.Previous;
+	    if (tail != null)
+	    {
+		    tail.Next = null;
+	    }
+	    else
+	    {
+		    head = null;
+	    }
+
+
+	    Count--;
+
+	    return valueToRemove;
+	}
+
+    public void ForEach(Action<T> action)
+    {
+	    while (head != null)
+	    {
+		    action(head.Value);
+		    head = head.Next;
+	    }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+	    while (head != null)
+	    {
+		    yield return head.Value;
+		    head = head.Next;
+	    }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+	    return this.GetEnumerator();
+    }
+
+    public T[] ToArray()
+    {
+        T[] array = new T[Count];
+	    int index = 0;
+	    while (head != null)
+	    {
+		    array[index] = head.Value;
+		    index++;
+		    head = head.Next;
+	    }
+
+	    return array;
+    }
+}
+
+
+class Example
+{
+    static void Main()
+    {
+        var list = new DoublyLinkedList<int>();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.AddLast(5);
+        list.AddFirst(3);
+        list.AddFirst(2);
+        list.AddLast(10);
+        Console.WriteLine("Count = {0}", list.Count);
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.RemoveFirst();
+        list.RemoveLast();
+        list.RemoveFirst();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.RemoveLast();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+    }
+}
