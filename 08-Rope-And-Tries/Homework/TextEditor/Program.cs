cddf9d940c34dc82cc209d3da7e7ac705bc85b9f
KostadinovK <kostadinovk2001@gmail.com>
2019-01-22 18:34:44 +0200

    Solved the homework to lecture 08

---
+++ 08-Rope-And-Tries/Homework/TextEditor/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        TextEditor editor = new TextEditor();
+        Dictionary<string, bool> users = new Dictionary<string, bool>();
+
+        string line = Console.ReadLine();
+        Regex regex = new Regex("\"(.*)\"");
+        while (line != "end")
+        {
+            string[] tokens = line.Split().ToArray();
+            Match match = regex.Match(line);
+
+            switch (tokens[0])
+            {
+                case "login":
+                    users[tokens[1]] = true;
+                    editor.Login(tokens[1]);
+                    break;
+                case "logout":
+                    users[tokens[1]] = false;
+                    editor.Logout(tokens[1]);
+                    break;
+                case "users":
+                    if (tokens.Length == 2)
+                    {
+                        editor.Users();
+                    }
+                    else
+                    {
+                        editor.Users(tokens[2]);
+                    }
+
+                    break;
+            }
+
+            if (!(users.ContainsKey(tokens[0]) && users[tokens[0]]))
+            {
+                line = Console.ReadLine();
+                continue;
+            }
+
+            switch (tokens[1])
+            {
+                case "insert":
+                    editor.Insert(tokens[0], int.Parse(tokens[2]), match.Groups[1].Value);
+                    break;
+                case "prepend":
+                    editor.Prepend(tokens[0], match.Groups[1].Value);
+                    break;
+                case "substring":
+                    editor.Substring(tokens[0],int.Parse(tokens[2]),int.Parse(tokens[3]));
+                    break;
+                case "delete":
+                    editor.Delete(tokens[0],int.Parse(tokens[2]),int.Parse(tokens[3]));
+                    break;
+                case "clear":
+                    editor.Clear(tokens[0]);
+                    break;
+                case "length":
+                    Console.WriteLine(editor.Length(tokens[0]));
+                    break;
+                case "print":
+                    Console.WriteLine(editor.Print(tokens[0]));
+                    break;
+                case "undo":
+                    editor.Undo(tokens[0]);
+                    break;
+            }
+
+            line = Console.ReadLine();
+        }
+    }
+}
---
+++ 08-Rope-And-Tries/Homework/TextEditor/TextEditor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class TextEditor : ITextEditor
+{
+    private Trie<StringBuilder> users;
+    private Trie<Stack<string>> cache;
+
+    public TextEditor()
+    {
+        users = new Trie<StringBuilder>();
+        cache = new Trie<Stack<string>>();
+    }
+
+    public void Login(string username)
+    {
+        users.Insert(username, new StringBuilder());
+        cache.Insert(username, new Stack<string>());
+    }
+
+    public void Logout(string username)
+    {
+        users.Delete(username);
+        cache.Delete(username);
+    }
+
+    public void Prepend(string username, string str)
+    {
+        this.Cache(username);
+
+        this.Insert(username,0,str);
+    }
+
+    public void Insert(string username, int index, string str)
+    {
+        this.Cache(username);
+
+        users.GetValue(username).Insert(index,str);
+    }
+
+    public void Substring(string username, int startIndex, int length)
+    {
+        this.Cache(username);
+
+        users.GetValue(username).Remove(0,startIndex);
+        users.GetValue(username).Remove(startIndex + length,users.GetValue(username).ToString().Length - startIndex + length);
+
+    }
+
+    public void Delete(string username, int startIndex, int length)
+    {
+        this.Cache(username);
+
+        users.GetValue(username).Remove(startIndex,length);
+    }
+
+    public void Clear(string username)
+    {
+        this.Cache(username);
+        users.GetValue(username).Clear();
+    }
+
+
+    public int Length(string username)
+    {
+        return users.GetValue(username).ToString().Length;
+    }
+
+    public string Print(string username)
+    {
+        return users.GetValue(username).ToString();
+    }
+
+    public void Undo(string username)
+    {
+        var undo = cache.GetValue(username);
+        if (undo.Count == 0)
+        {
+            return;
+        }
+
+        users.Insert(username,new StringBuilder(undo.Pop()));
+
+    }
+
+    public IEnumerable<string> Users(string prefix = "")
+    {
+        return users.GetByPrefix(prefix);
+    }
+
+    private void Cache(string username)
+    {
+        cache.GetValue(username).Push(users.GetValue(username).ToString());
+    }
+}
+
