80d349bcd1a25b451d9865bd568ace39d647a767
KostadinovK <kostadinovk2001@gmail.com>
2019-01-26 17:56:13 +0200

    Added homework description for lecture 09 and solved 01.SweepAndPrune from it

---
+++ 09-Quad-Trees-K_D-Trees-And-Interval-Trees/Homework/SweepAndPrune/SweepAndPrune/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GameObject
+{
+    public string Name { get; set; }
+    public int X1 { get; set; }
+    public int Y1 { get; set; }
+    public int X2 { get; set; }
+    public int Y2 { get; set; }
+
+    public GameObject(string name, int x1, int y1)
+    {
+        Name = name;
+        X1 = x1;
+        Y1 = y1;
+
+        X2 = X1 + 10;
+        Y2 = Y1 + 10;
+    }
+
+    public void Move(int newX1, int newY1)
+    {
+        X1 = newX1;
+        Y1 = newY1;
+
+        X2 = X1 + 10;
+        Y2 = Y1 + 10;
+    }
+}
+
---
+++ 09-Quad-Trees-K_D-Trees-And-Interval-Trees/Homework/SweepAndPrune/SweepAndPrune/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<GameObject> objects = new List<GameObject>();
+
+        string line = Console.ReadLine();
+
+        while (line != "start")
+        {
+            string[] tokens = line.Split().ToArray();
+            if (tokens[0] == "add")
+            {
+                GameObject obj = new GameObject(tokens[1], int.Parse(tokens[2]), int.Parse(tokens[3]));
+                if (!objects.Contains(obj))
+                {
+                    objects.Add(obj);
+                }
+            }
+
+            line = Console.ReadLine();
+        }
+
+        objects.Sort((x,y) => x.X1.CompareTo(y.X1));
+        line = Console.ReadLine();
+
+        int ticks = 1;
+        while (line != "end")
+        {
+            string[] tokens = line.Split().ToArray();
+            if (tokens[0] == "move")
+            {
+                GameObject obj = GetObject(objects, tokens[1]);
+
+                obj?.Move(int.Parse(tokens[2]), int.Parse(tokens[3]));
+
+                objects.Sort((x,y) => x.X1.CompareTo(y.X1));
+            }
+
+            CheckForCollisions(ticks, objects);
+
+            ticks++;
+            line = Console.ReadLine();
+        }
+    }
+
+    private static void CheckForCollisions(int ticks, List<GameObject> objects)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            for (int j = i + 1; j < objects.Count; j++)
+            {
+                if (objects[i].X2 < objects[j].X1)
+                {
+                    break;
+                }
+
+                if (objects[i].Y1 <= objects[j].Y2 && objects[i].Y2 >= objects[j].Y1)
+                {
+                    Console.WriteLine($"({ticks}) {objects[i].Name} collides with {objects[j].Name}");
+                }
+            }
+        }
+    }
+
+    private static GameObject GetObject(List<GameObject> objects, string name)
+    {
+        for (int i = 0; i < objects.Count; i++)
+        {
+            if (name == objects[i].Name)
+            {
+                return objects[i];
+            }
+        }
+
+        return null;
+    }
+}
+
