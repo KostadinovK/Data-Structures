a5f7d648959998fe97a622b8ce0e066b9113973b
KostadinovK <kostadinovk2001@gmail.com>
2019-02-13 11:44:58 +0200

    Solved the first task from the exam on 11.03.2018

---
+++ 12-Exam-Prep/Exam-11.03.2018/01-INstock/PeshoAndCo/Instock.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Instock : IProductStock
+{
+    private List<Product> products;
+    private Dictionary<string, Product> byLabel;
+    private Dictionary<int, HashSet<Product>> byQuantity;
+
+
+    public Instock()
+    {
+        products = new List<Product>();
+        byLabel = new Dictionary<string, Product>();
+        byQuantity = new Dictionary<int, HashSet<Product>>();
+    }
+
+    public int Count => products.Count;
+
+    public void Add(Product product)
+    {
+        products.Add(product);
+        if (!byLabel.ContainsKey(product.Label))
+        {
+            byLabel.Add(product.Label, product);
+            
+        }
+
+        if (!byQuantity.ContainsKey(product.Quantity))
+        {
+            byQuantity[product.Quantity] = new HashSet<Product>();
+        }
+
+        byQuantity[product.Quantity].Add(product);
+
+    }
+
+    public void ChangeQuantity(string product, int quantity)
+    {
+        if (!byLabel.ContainsKey(product))
+        {
+            throw new ArgumentException();
+        }
+
+        var p = byLabel[product];
+        this.byQuantity[p.Quantity].Remove(p);
+
+        if (!this.byQuantity.ContainsKey(quantity))
+        {
+            this.byQuantity[quantity] = new HashSet<Product>();
+        }
+        p.Quantity = quantity;
+
+        this.byQuantity[quantity].Add(p);
+    }
+
+    public bool Contains(Product product)
+    {
+        return byLabel.ContainsKey(product.Label);
+    }
+
+    public Product Find(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return products[index];
+    }
+
+    public IEnumerable<Product> FindAllByPrice(double price)
+    {
+        return products.Where(x => x.Price == price);
+    }
+
+    public IEnumerable<Product> FindAllByQuantity(int quantity)
+    {
+        if (!byQuantity.ContainsKey(quantity))
+        {
+            return new List<Product>();
+        }
+
+        return byQuantity[quantity];
+    }
+
+    public IEnumerable<Product> FindAllInRange(double lo, double hi)
+    {
+        return products.Where(x => x.Price > lo && x.Price <= hi).OrderByDescending(x => x.Price);
+    }
+
+    public Product FindByLabel(string label)
+    {
+        if (!byLabel.ContainsKey(label))
+        {
+            throw new ArgumentException();
+        }
+
+        return byLabel[label];
+    }
+
+    public IEnumerable<Product> FindFirstByAlphabeticalOrder(int count)
+    {
+        if (count < 0 || count > Count)
+        {
+            throw new ArgumentException();
+        }
+
+        return byLabel.Values.Take(count);
+    }
+
+    public IEnumerable<Product> FindFirstMostExpensiveProducts(int count)
+    {
+        if (count < 0 || count > Count)
+        {
+            throw new ArgumentException();
+        }
+
+        return products.OrderByDescending(x => x.Price).Take(count);
+    }
+
+    public IEnumerator<Product> GetEnumerator()
+    {
+        foreach (var product in products)
+        {
+            yield return product;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ 12-Exam-Prep/Exam-11.03.2018/02-Chainblock/Chainblock/Chainblock.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class Chainblock : IChainblock
+{
+    public int Count => throw new NotImplementedException();
+
+    public void Add(Transaction tx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(Transaction tx)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetAllInAmountRange(double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetAllOrderedByAmountDescendingThenById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Transaction GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetByReceiverOrderedByAmountThenById(string receiver)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetBySenderOrderedByAmountDescending(string sender)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetByTransactionStatus(TransactionStatus status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Transaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<Transaction> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveTransactionById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
+
