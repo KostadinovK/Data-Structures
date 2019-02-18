cfaf83381313348c7740657a91cce9598303df0f
KostadinovK <kostadinovk2001@gmail.com>
2019-02-18 10:28:22 +0200

    Solved the first task from the exam on 20.05.2018

---
+++ 12-Exam-Prep/Exam-20.05.2018/01-Scheduler/ThreadExecutor/ThreadExecutor.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+/// <summary>
+/// The ThreadExecutor is the concrete implementation of the IScheduler.
+/// You can send any class to the judge system as long as it implements
+/// the IScheduler interface. The Tests do not contain any <e>Reflection</e>!
+/// </summary>
+public class ThreadExecutor : IScheduler
+{
+    private int cycles;
+    private Dictionary<int, Task> byId;
+    private List<Task> byInsertion;
+  
+    public ThreadExecutor()
+    {
+        byId = new Dictionary<int, Task>();
+        byInsertion = new List<Task>();
+        cycles = 0;
+    }
+
+    public int Count => byId.Count;
+
+    public void Execute(Task task)
+    {
+        if (byId.ContainsKey(task.Id))
+        {
+            throw new ArgumentException();
+        }
+
+        byId.Add(task.Id, task);
+        byInsertion.Add(task);
+    }
+
+    public bool Contains(Task task)
+    {
+        return byId.ContainsKey(task.Id);
+    }
+
+    public Task GetById(int id)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+
+        return byId[id];
+    }
+
+    public Task GetByIndex(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+        return byInsertion[index];
+    }
+
+    public int Cycle(int cycles)
+    {
+        if (Count == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        List<Task> toRemove = new List<Task>();
+
+        this.cycles += cycles;
+
+        foreach (var task in byInsertion)
+        {
+
+            if (task.Consumption <= this.cycles)
+            {
+                toRemove.Add(task);
+            }
+        }
+
+        toRemove.ForEach(x => byId.Remove(x.Id));
+        toRemove.ForEach(x => byInsertion.Remove(x));
+       
+        return toRemove.Count;
+    }
+
+    public void ChangePriority(int id, Priority newPriority)
+    {
+        if (!byId.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+
+        byId[id].TaskPriority = newPriority;
+
+    }
+
+    public IEnumerable<Task> GetByConsumptionRange(int lo, int hi, bool inclusive)
+    {
+        if (inclusive)
+        {
+            return byId.Values.Where(x => x.Consumption >= lo + cycles && x.Consumption <= hi + cycles).OrderBy(x => x);
+        }
+
+        return byId.Values.Where(x => x.Consumption > lo + cycles && x.Consumption < hi + cycles).OrderBy(x => x);
+    }
+
+    public IEnumerable<Task> GetByPriority(Priority type)
+    {
+        return byId.Values.Where(x => x.TaskPriority == type).OrderByDescending(x => x.Id);
+    }
+
+    public IEnumerable<Task> GetByPriorityAndMinimumConsumption(Priority priority, int lo)
+    {
+        return byId.Values.Where(x => x.TaskPriority == priority && x.Consumption >= lo).OrderByDescending(x => x.Id);
+    }
+
+    public IEnumerator<Task> GetEnumerator()
+    {
+        foreach (var task in byInsertion)
+        {
+            yield return task;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
---
+++ 12-Exam-Prep/Exam-20.05.2018/02-RoyalArena/RoyaleArena/RoyaleArena.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class RoyaleArena : IArena
+{
+    public int Count => throw new NotImplementedException();
+
+    public void Add(Battlecard card)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ChangeCardType(int id, CardType type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Contains(Battlecard card)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> FindFirstLeastSwag(int n)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetAllByNameAndSwag()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetAllInSwagRange(double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetByCardType(CardType type)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetByCardTypeAndMaximumDamage(CardType type, double damage)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Battlecard GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetByNameAndSwagRange(string name, double lo, double hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetByNameOrderedBySwagDescending(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Battlecard> GetByTypeAndDamageRangeOrderedByDamageThenById(CardType type, int lo, int hi)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<Battlecard> GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
